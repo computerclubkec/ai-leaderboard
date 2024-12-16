c1d2914fd5a6714341219d8c4b39de8cdbb7c7bc
gaurovgiri <gaurovgiri@gmail.com>
2024-12-17 00:28:54 +0545

    add facebook scraper

---
+++ fb-scraper/scrapeboard.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.safari.options import Options
+import re
+import csv
+import json
+
+class FacebookPostScraper:
+    def __init__(self, csv_file):
+        self.csv_file = csv_file
+        self.final_data = []
+
+        # Set up headless Safari options
+        options = Options()
+        options.headless = True
+
+        self.driver = webdriver.Safari(options=options)
+
+    def open_post(self, url):
+        """Open a Facebook post URL."""
+        self.driver.get(url)
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "x5yr21d.x4l50q0"))
+            )
+        except Exception as e:
+            print(f"Error loading post: {e}")
+        finally:
+            webdriver.ActionChains(self.driver).send_keys(Keys.ESCAPE).perform()
+
+    @staticmethod
+    def calculate_points(likes, shares):
+        """Calculate points based on likes and shares."""
+        return likes * 2 + shares * 5
+
+    def scrape(self):
+        """Scrape data from the list of Facebook post links."""
+        with open(self.csv_file, "r") as file:
+            csv_reader = csv.DictReader(file)
+
+            for row in csv_reader:
+                url = row.get("link", "").strip()
+                author = row.get("author", "Unknown")
+                text = row.get("text", "")
+
+                if not url:
+                    print("Skipping row with no link.")
+                    continue
+
+                self.open_post(url)
+
+                try:
+                    reactions_element = self.driver.find_element(
+                        By.CSS_SELECTOR,
+                        "div.x1n2onr6 > div > div.x6s0dn4.x78zum5.x1iyjqo2.x6ikm8r.x10wlt62 > span.xrbpyxo.x6ikm8r.x10wlt62.xlyipyv.x1exxlbk > span > span"
+                    )
+                    no_of_reactions = int(reactions_element.text.replace(",", ""))
+                except Exception:
+                    no_of_reactions = 0
+
+                try:
+                    shares_element = self.driver.find_element(
+                        By.CSS_SELECTOR,
+                        "div.x1n2onr6 > div > div.x9f619.x1ja2u2z.x78zum5.x2lah0s.x1n2onr6.x1qughib.x1qjc9v5.xozqiw3.x1q0g3np.xykv574.xbmpl8g.x4cne27.xifccgj > div:nth-child(2) > span > div > div > div:nth-child(1) > span > span"
+                    )
+                    shares_text = shares_element.text
+                    if "K" in shares_text:
+                        no_of_shares = int(float(shares_text.replace("K", "")) * 1000)
+                    else:
+                        no_of_shares = int(shares_text.replace(",", ""))
+                except Exception:
+                    no_of_shares = 0
+
+                try:
+                    image_element = self.driver.find_element(
+                        By.CSS_SELECTOR,
+                        "div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xtp0wl1.xadl8oe > div > div.x78zum5.xdt5ytf.x1iyjqo2.x5yr21d.x17upfok.x1n2onr6.x1uvtmcs.xal61yo > div > div.x6s0dn4.x78zum5.x1n2onr6.xtp0wl1.xwot789 > div > div > div > img"
+                    )
+                    image_url = image_element.get_attribute("src")
+                except Exception:
+                    image_url = ""
+
+                points = self.calculate_points(no_of_reactions, no_of_shares)
+
+                print(f"Link: {url}, Reactions: {no_of_reactions}, Shares: {no_of_shares}, Points: {points}")
+
+                self.final_data.append({
+                    "Author": author,
+                    "Text": text,
+                    "PostLink": url,
+                    "NumberOfReactions": no_of_reactions,
+                    "NumberOfShares": no_of_shares,
+                    "Image": image_url,
+                    "Points": points,
+                })
+
+        return self.final_data
+
+    def save_to_json(self, output_file):
+        """Save scraped data to a JSON file."""
+        with open(output_file, "w") as json_file:
+            json.dump({"posts": self.final_data}, json_file, indent=4)
+
+    def close(self):
+        """Close the WebDriver."""
+        self.driver.quit()
+
+if __name__ == "__main__":
+    scraper = FacebookPostScraper("links.csv")
+    try:
+        data = scraper.scrape()
+        scraper.save_to_json("data.json")
+    finally:
+        scraper.close()
